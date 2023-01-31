71d8dad36a48c1091a0586e9c40d684c3baa8ceb
mrkvon <mrkvon@protonmail.com>
2023-01-31 16:15:01 +0100

    Add prettier and eslint, format code
    Also make src a baseUrl in import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import logo from './logo.svg'
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +19,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
