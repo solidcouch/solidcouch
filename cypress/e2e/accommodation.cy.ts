a9e8c958e481cfbea34759343c25b5cee8b19e30
mrkvon <mrkvon@protonmail.com>
2023-06-14 19:36:11 +0200

    Draft pending tests for various features

---
+++ cypress/e2e/accommodation.cy.ts
@@ -0,0 +1,7 @@
+describe('accommodations offered by person', () => {
+  it('[other person] should be able to see accommodations of person') // not sure about this
+  it('[me] should be able to see accommodations i offer')
+  it('[me] should be able to edit location and description of accommodation')
+  it('[me] should be able to delete accommodation')
+  it('[me] should be able to add new accommodation')
+})
