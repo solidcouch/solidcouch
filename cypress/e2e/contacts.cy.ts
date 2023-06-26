a0ab6ea1ee61a63a548e92e951fee22e23ab6c9d
mrkvon <mrkvon@protonmail.com>
2023-06-26 20:08:19 +0200

    Write passing tests for adding contact and sending contact request

--- cypress/e2e/contacts.cy.ts
+++ cypress/e2e/contacts.cy.ts
@@ -17,13 +17,23 @@ describe("person's contacts", () => {
         cy.get<Person>('@person2').then(person2 => {
           cy.get<Person>('@person3').then(person3 => {
             cy.get<Person>('@person4').then(person4 => {
-              cy.saveContacts({ person: me, contacts: [] })
+              cy.saveContacts({
+                person: me,
+                // person1 - full contact
+                // person2 - only contact from me
+                // person3 - only contact to me (implement notification!)
+                // person4 - no contact
+                contacts: [person1.webId, person2.webId],
+              })
               cy.saveContacts({
                 person: person1,
                 contacts: [person2.webId, person3.webId, person4.webId],
               })
               cy.saveContacts({ person: person2, contacts: [person1.webId] })
-              cy.saveContacts({ person: person3, contacts: [person1.webId] })
+              cy.saveContacts({
+                person: person3,
+                contacts: [me.webId, person1.webId],
+              })
               cy.saveContacts({ person: person4, contacts: [] })
             })
           })
@@ -66,9 +76,29 @@ describe("person's contacts", () => {
     })
   })
 
-  it(
-    'should allow adding other person as contact and send contact request notification',
-  )
+  it('should allow adding other person as contact and send contact request notification', () => {
+    cy.get<Person>('@person4').then(person4 => {
+      cy.visit(`/profile/${encodeURIComponent(person4.webId)}`)
+
+      cy.intercept({ method: 'POST', url: person4.inbox }).as(
+        'sendNotification',
+      )
+      // there should be button add as contact
+      cy.contains('button', 'Add to my contacts').click()
+      cy.get('textarea[name=invitation]')
+        .clear()
+        .type('Contact invitation text')
+      cy.contains('button', 'Send contact invitation').click()
+      // wait for the button to update
+      cy.contains('Contact request sent')
+      // check that notification was sent
+      cy.wait('@sendNotification')
+        .its('request.body')
+        .should('contain', 'Contact invitation text')
+    })
+  })
+
+  it('should give contacts access to my hospex')
 
   it('should allow removing other person as contact')
 
