ac85c9542cd74f4fd69f0efcf38928b9b2014edf
mrkvon <mrkvon@protonmail.com>
2023-07-22 02:38:50 +0300

    Set up static ClientID at clientid.jsonld
    Creates new environment variables:
    `BASE_URL` - the app url without trailing slash that is used in ClientID
    `REACT_APP_ENABLE_DEV_CLIENT_ID` - enable static ClientID in development

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -38,4 +38,20 @@ describe('Sign in to the app', () => {
     })
     cy.contains('We would like to set up your Pod')
   })
+
+  it('should use provided ClientID for sign-in', () => {
+    cy.visit('/')
+    cy.contains('Sign in').click()
+    cy.get<UserConfig>('@user1').then(user1 => {
+      cy.get('input[name=webIdOrIssuer]').type(`${user1.idp}{enter}`)
+      cy.origin(user1.idp, { args: { user1 } }, ({ user1 }) => {
+        cy.get('input[name=email]').type(user1.email)
+        cy.get('input[name=password]').type(`${user1.password}{enter}`)
+        // check that clientid.jsonld is used as ID
+        cy.contains('clientid.jsonld')
+        cy.get('button#authorize').click()
+      })
+    })
+    cy.contains('We would like to set up your Pod')
+  })
 })
