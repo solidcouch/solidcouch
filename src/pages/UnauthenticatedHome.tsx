8ca94defd88c4d25e2f81f57d7cb242971aaaa6f
mrkvon <mrkvon@protonmail.com>
2023-07-25 00:46:46 +0300

    Add info about sleepy.bike to front page and meta description
    Co-authored-by: Maria Kozinska <m.kozinska@gmail.com>

--- src/pages/UnauthenticatedHome.tsx
+++ src/pages/UnauthenticatedHome.tsx
@@ -1,13 +1,55 @@
-import { ButtonLink } from 'components'
 import { SignIn } from 'components/SignIn/SignIn'
+import styles from './UnauthenticatedHome.module.scss'
 
 export const UnauthenticatedHome = () => {
   return (
-    <div>
-      <SignIn />
-      <ButtonLink tertiary to="about">
+    <div className={styles.wrapper}>
+      <section className={styles.mainDescription}>
+        Sleepy Bike is a community of bicycle touring travellers and those who
+        want to host them.
+      </section>
+      <section className={styles.actions}>
+        <SignIn />
+      </section>
+      <div className={styles.projects}>
+        <a href="https://openhospitality.network" className={styles.project}>
+          <img
+            src="https://openhospitality.network/assets/img/logo.png"
+            title="Open Hospitality Network"
+            alt="Open Hospitality Network logo"
+            className={styles.logo}
+          />
+          <div>Open Hospitality Network</div>
+        </a>
+        <span className={styles.plus}>+</span>
+        <a href="https://solidproject.org" className={styles.project}>
+          <img
+            src="https://avatars3.githubusercontent.com/u/14262490?v=3&s=200"
+            title="Solid Project"
+            alt="Solid Project logo"
+            className={styles.logo}
+          />
+          <div>Solid Project</div>
+        </a>
+      </div>
+      {/*<!--As a member, you own your data. In the future, you'll be able to
+          connect with other similar communities in the greater hospitality
+  exchange network.-->*/}
+      {/*<ButtonLink tertiary to="about">
         Read more
-      </ButtonLink>
+</ButtonLink>*/}
+      <div className={styles.spacer} />
+      <footer className={styles.footer}>
+        Visit our project spaces to{' '}
+        <a href="https://matrix.to/#/#ohn:matrix.org">
+          ask a question or join the team
+        </a>
+        ,{' '}
+        <a href="https://github.com/openHospitalityNetwork/sleepy.bike">
+          view source code
+        </a>{' '}
+        or <a href="https://opencollective.com/ohn">support us financially</a>.
+      </footer>
     </div>
   )
 }
