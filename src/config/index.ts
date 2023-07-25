ff8b2d99f1c4deb1aee8fecd1c4986b831f7d7c6
mrkvon <mrkvon@protonmail.com>
2023-07-25 21:01:31 +0300

    Add configuration option for community data container (#47)
    Also add related documentation

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,14 @@ export const communityId =
   process.env.REACT_APP_COMMUNITY ||
   'https://solidweb.me/dev-sleepy-bike/community#us'
 
+/**
+ * Name of folder in which to store person's data of this community
+ *
+ * During setup, the container with this community's data will be, by default, created at {storage}/hospex/{communityContainer}/
+ * Otherwise this doesn't matter
+ * Don't include trailing slash!
+ */
+export const communityContainer =
+  process.env.REACT_APP_COMMUNITY_CONTAINER || 'dev-sleepy-bike'
+
 export const wikidataLDF = 'https://query.wikidata.org/bigdata/ldf'
