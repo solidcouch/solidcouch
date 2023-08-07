17b8f06f782e567c94b2db00120e36fcd83915d1
mrkvon <mrkvon@protonmail.com>
2023-08-07 18:08:25 +0300

    Shorten the project description for search engines (#50)
    Also improve layout of landing page logos on tiny screens
    
    Before, a major search engine truncated the meta description

--- src/pages/UnauthenticatedHome.tsx
+++ src/pages/UnauthenticatedHome.tsx
@@ -1,13 +1,19 @@
 import { ButtonLink } from 'components'
 import { SignIn } from 'components/SignIn/SignIn'
+import styles from './UnauthenticatedHome.module.scss'
 
 export const UnauthenticatedHome = () => {
   return (
-    <div>
-      <SignIn />
+    <div className={styles.wrapper}>
+      <section className={styles.mainDescription}></section>
+      <section className={styles.actions}>
+        <SignIn />
+      </section>
       <ButtonLink tertiary to="about">
         Read more
       </ButtonLink>
+      <div className={styles.spacer} />
+      <footer className={styles.footer}></footer>
     </div>
   )
 }
