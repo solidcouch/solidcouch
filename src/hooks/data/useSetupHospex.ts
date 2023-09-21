cc9f2a641fc68c8fc8df7a16858bf085d8173ae0
mrkvon <mrkvon@protonmail.com>
2023-09-21 22:44:09 +0200

    Test whether warnings also fail

--- src/hooks/data/useSetupHospex.ts
+++ src/hooks/data/useSetupHospex.ts
@@ -32,6 +32,7 @@ export type SetupSettings = {
   hospexDocument: URI
 }
 
+
 export const useSetupHospex = () => {
   const createPrivateTypeIndex = useCreatePrivateTypeIndex()
   const createPublicTypeIndex = useCreatePublicTypeIndex()
