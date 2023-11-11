72fded33220a5b239ab44431bcb447095f23da51
mrkvon <mrkvon@protonmail.com>
2023-11-11 14:17:29 +0100

    Add a progress bar to map of accommodations (#67)
    It indicates data fetching in progress
    
    New component `ProgressBar` can be animated with property `animate`
    Usage: `<ProgressBar />` or `<ProgressBar animate />`
    
    Notice the thin gray line on top: that's the non-animated version that
    we use for this page.

--- src/pages/SearchHosts/SearchHosts.tsx
+++ src/pages/SearchHosts/SearchHosts.tsx
@@ -1,4 +1,5 @@
 import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
+import { ProgressBar } from 'components/ProgressBar/ProgressBar'
 import { defaultIcon, highlightedIcon, tileServer } from 'config'
 import { useSearchAccommodations } from 'hooks/data/useSearchAccommodations'
 import { FaTimes } from 'react-icons/fa'
@@ -16,62 +17,64 @@ export const SearchHosts = () => {
 
   //const offers: AccommodationExtended[] = []
 
-  const [offers] = useSearchAccommodations()
+  const [offers, status] = useSearchAccommodations()
 
   const handleMarkerClick = (accommodationId: URI) => {
     setSearchParams({ hosting: accommodationId })
   }
 
   return (
-    <div className={styles.container}>
-      {/* {isLoading && <div>...</div>} */}
-      {selectedAccommodationId && (
-        <div className={styles.offerOverlay}>
-          <button
-            onClick={() => {
-              setSearchParams({})
-            }}
-            className={styles.closeButton}
-            aria-label="Close hosting preview"
-          >
-            <FaTimes />
-          </button>
-          <AccommodationInfo accommodationId={selectedAccommodationId} />
-        </div>
-      )}
-      <MapContainer
-        className={styles.mapContainer}
-        attributionControl={false}
-        zoom={1}
-        center={[0, 0]}
-      >
-        <TileLayer url={tileServer} />
-        <MarkerClusterGroup maxClusterRadius={20}>
-          {offers
-            ? offers.map(offer => {
-                return (
-                  <Marker
-                    key={offer.id}
-                    position={[offer.location.lat, offer.location.long]}
-                    eventHandlers={{
-                      click: () => {
-                        handleMarkerClick(offer.id)
-                      },
-                    }}
-                    icon={
-                      offer.id === selectedAccommodationId
-                        ? highlightedIcon
-                        : defaultIcon
-                    }
-                    alt={`Accommodation offer from ${
-                      offer.offeredBy?.name || offer.offeredBy?.id
-                    }`}
-                  />
-                )
-              })
-            : null}
-        </MarkerClusterGroup>
-      </MapContainer>
-    </div>
+    <>
+      {status.isLoading && <ProgressBar />}
+      <div className={styles.container}>
+        {selectedAccommodationId && (
+          <div className={styles.offerOverlay}>
+            <button
+              onClick={() => {
+                setSearchParams({})
+              }}
+              className={styles.closeButton}
+              aria-label="Close hosting preview"
+            >
+              <FaTimes />
+            </button>
+            <AccommodationInfo accommodationId={selectedAccommodationId} />
+          </div>
+        )}
+        <MapContainer
+          className={styles.mapContainer}
+          attributionControl={false}
+          zoom={1}
+          center={[0, 0]}
+        >
+          <TileLayer url={tileServer} />
+          <MarkerClusterGroup maxClusterRadius={20}>
+            {offers
+              ? offers.map(offer => {
+                  return (
+                    <Marker
+                      key={offer.id}
+                      position={[offer.location.lat, offer.location.long]}
+                      eventHandlers={{
+                        click: () => {
+                          handleMarkerClick(offer.id)
+                        },
+                      }}
+                      icon={
+                        offer.id === selectedAccommodationId
+                          ? highlightedIcon
+                          : defaultIcon
+                      }
+                      alt={`Accommodation offer from ${
+                        offer.offeredBy?.name || offer.offeredBy?.id
+                      }`}
+                    />
+                  )
+                })
+              : null}
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
+    </>
   )
 }
