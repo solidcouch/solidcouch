f663e24b199d16ca0057a5a1df543d1f8d29624d
mrkvon <mrkvon@protonmail.com>
2023-11-21 21:56:24 +0100

    Change content and fix link

--- src/pages/About/about.ts
+++ src/pages/About/about.ts
@@ -1,10 +1,10 @@
-export const about = `# The Sleepy Bike Community
+export const about = `# About
 
 Welcome to the sleepy bike community. We're happy to have you. Here you can read why we created it and what our values are. See if it matches and you feel it would fit your way of travelling and connecting.
 
 ## What
 
-The sleepy bike community is created for the bicycle touring traveller and their supporters. Those who roam by bike, camp out and stay off grid but also like to connect with others, enjoy some nice hot food and a warm shower along the way. It's decentralised and open source. Our main values are 'transparency, democracy and passion for connection'.
+The sleepy bike community is created for the bicycle touring traveller and their supporters. Those who roam by bike, camp out and stay off grid but also like to connect with others, enjoy some nice hot food and a warm shower along the way. It's decentralised and open source. Our main values are 'transparency, democracy and mutual aid'.
 
 ## Why
 
@@ -12,7 +12,7 @@ Because we believe that building a community with Solid as a base gives users mo
 
 ## How
 
-The use of sleepy bike is very straightforward. It has all the functions to search for hosts, create a profile, send messages and add contacts. Basically, we go back to the core of hospex: you meet/look for people, start a conversation to see if it's a match and add them to your contacts afterward. The design focuses on function over looks, which means less complicated code and therefore less maintenance and sensitivity to crashing. To find more info about the server Solid, privacy, etc., check out [their website](https://solidcommunity.net). You can check out the sleepy bike progress at [this GitHub](https://github.com/OpenHospitalityNetwork/sleepy.bike). If you want to join the conversation, you can find us on [Matrix](https://matrix.to/#/#ohn:matrix.org).
+The use of sleepy bike is very straightforward. It has all the functions to search for hosts, create a profile, send messages and add contacts. The design focuses on function over looks, which means less complicated code and therefore less maintenance and sensitivity to crashing. To find more info about the server Solid, privacy, etc., check out [their website](https://solidproject.org). You can check out the sleepy bike progress at [this GitHub](https://github.com/OpenHospitalityNetwork/sleepy.bike). If you want to join the conversation, you can find us on [Matrix](https://matrix.to/#/#ohn:matrix.org).
 
 ## Who
 
