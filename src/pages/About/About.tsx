f157555937bbbe68c58c9d0427b7680bdb60ed53
petr-hajek <38785076+petr-hajek@users.noreply.github.com>
2023-12-13 21:50:09 +0100

    Fix about page (#76)
    This fixed the compiling issue with the about page
    
    ---------
    
    Co-authored-by:  Petr Hajek <petr.banan.hajek@gmail.com>

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,5 @@
 import Markdown from 'react-markdown'
 import styles from './About.module.scss'
-import { about } from './about'
+import { about } from './data'
 
 export const About = () => <Markdown className={styles.text}>{about}</Markdown>
