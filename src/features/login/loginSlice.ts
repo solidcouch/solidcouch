ccab4ba96283c3e9057d8a9946087070ba2b1401
mrkvon <mrkvon@protonmail.com>
2024-04-06 15:22:45 +0200

    Remember last selected identity provider
    Store it in localStorage using redux-persist
    This should help people remember their Solid provider
    and not have to type their custom provider every time

---
+++ src/features/login/loginSlice.ts
@@ -0,0 +1,24 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { RootState } from 'app/store'
+
+interface LoginState {
+  lastSelectedIssuer?: string
+}
+
+const initialState: LoginState = {}
+
+export const slice = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {
+    setLastSelectedIssuer: (state, action: PayloadAction<string>) => {
+      state.lastSelectedIssuer = action.payload
+    },
+  },
+})
+
+export const { actions, reducer } = slice
+
+export const selectLastSelectedIssuer = (state: RootState) =>
+  state.login.lastSelectedIssuer
