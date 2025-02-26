6365b9d27e682f42388333736bf8827f8f7ef73a
mrkvon <mrkvon@protonmail.com>
2025-02-26 07:27:28 +0100

    i18n fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-default-export
 export default {
   '*': [
-    'yarn i18n:extract',
-    'yarn i18n:compile',
+    () => 'yarn i18n:extract',
+    () => 'yarn i18n:compile',
     'prettier --write --ignore-unknown',
     () => 'knip',
   ],
-  '*.{js,jsx,ts,tsx}': 'eslint --max-warnings 0',
+  '*.{js,jsx,ts,tsx}': 'eslint --max-warnings 0 --no-warn-ignored',
 }
