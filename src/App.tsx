46dc9c6cc08f4d37a0aa9575a1a741921b438a1b
mrkvon <mrkvon@protonmail.com>
2025-12-18 12:45:50 +0100

    fix: return to previous url after page reload

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,11 @@ export const App = () => {
 
   useEffect(() => {
     ;(async () => {
+      const currentUrl = globalThis.location.href
+      // eslint-disable-next-line lingui/no-unlocalized-strings
+      if (!currentUrl.includes('?code='))
+        // eslint-disable-next-line lingui/no-unlocalized-strings
+        globalThis.localStorage.setItem('previousUrl', currentUrl)
       const session = await handleIncomingRedirect({
         restorePreviousSession: true,
       })
