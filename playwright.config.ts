78dc77bc769142ce0bdd47369a1ddc8c44812f52
mrkvon <mrkvon@protonmail.com>
2026-01-15 15:15:49 +0100

    ci: fix artifact uploads in matrix

--- playwright.config.ts
+++ playwright.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
 
     /* Test against mobile viewports. */
     {
-      name: 'Mobile Chrome',
+      name: 'mobile-chrome',
       use: { ...devices['Pixel 5'] },
     },
     // {
